e0ab8f62653646b769e1398baa0d2e2a54ac3327
Jennifer Penuelas <penuelas.jen@gmail.com>
2018-02-05 20:11:21 -0800

    Fix bug in script.js referencing the "click_counter" id in the HTML

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-
 $(document).ready(function() {
 
     $("#cat_pic").on('click', increment);
     function increment() {
-        var counter = parseInt($("#click-count").text());
+        var counter = parseInt($("#click_count").text());
         counter++;
-        $("#click-count").text(counter)
+        $("#click_count").text(counter)
         alert('You have clicked: ' + counter + ' times!');
-    });
+    };
+
 });
